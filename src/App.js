9f4cde0d953742394c70be60b66ae5196c03d247
Hardik981 <41016443+Hardik981@users.noreply.github.com>
2023-01-04 03:57:29 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  var name = 'Child';
+  return <Child name={name} />;
+}
+
+function Child(props) {
+  return <h1>Hello I am from {props.name}</h1>;
 }
